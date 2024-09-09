68e63a275f0fcfe23420c371c1ffbee0413f9fce
Dmitry Dolgopolov <dimadolgopolovn@gmail.com>
2024-09-09 05:40:55 -0400

    Sliding sidebar rewrite

--- src/renderer/Views/Main.tsx
+++ src/renderer/Views/Main.tsx
@@ -1,27 +1,20 @@
-import React, { useRef, useState } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import styled from '@emotion/styled'
 import ChatList from '../Components/ChatList'
 import MessageList from '../Components/MessageList'
 import { Button, Input } from 'react-chat-elements'
 
+// I tried using https://github.com/bvaughn/react-resizable-panels from _shadcn_ but they don't support px default width.
+// I tried implementing it but it proved to be too complicated, so I went for this library instead.
+// https://github.com/tomkp/react-split-pane doesn't work at all. It's too buggy.
+import { Allotment } from 'allotment'
+import 'allotment/dist/style.css'
+// TODO Dima: Types are wrong. Allotment.Pane.maxSize supports strings too. Same with defaultSizes
+
 // Styled Components
 const MainContainer = styled.div`
   display: flex;
   height: 100vh;
-`
-
-const ResizableSidebar = styled.div<{ width: number }>`
-  width: ${({ width }) => width}px;
-  min-width: 200px;
-  max-width: 500px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  background-color: #fff;
-  color: white;
-  border-right: 1px solid #3a3a3a;
-  resize: horizontal;
-  overflow: auto;
 `
 
 const ChatViewContainer = styled.div`
@@ -41,7 +34,6 @@ const InputContainer = styled.div`
 `
 
 const MainChatApp: React.FC = () => {
-  const [sidebarWidth, setSidebarWidth] = useState(300)
   const inputRef = useRef(null)
   const inputClearRef = useRef<Function | null>(null)
 
@@ -49,42 +41,48 @@ const MainChatApp: React.FC = () => {
 
   return (
     <MainContainer>
-      <ResizableSidebar
-        width={sidebarWidth}
-        onMouseUp={(e) => setSidebarWidth(e.currentTarget.clientWidth)}
-      >
-        <ChatList />
-      </ResizableSidebar>
-      <ChatViewContainer>
-        <MessageListContainer>
-          <MessageList />
-        </MessageListContainer>
-        <InputContainer>
-          <Input
-            className="rce-example-input"
-            placeholder="Write a message..."
-            defaultValue=""
-            multiline={true}
-            maxlength={1000}
-            onMaxLengthExceed={() => console.log('onMaxLengthExceed')}
-            reference={inputRef}
-            clear={(clear: any) => (inputClearRef.current = clear)}
-            maxHeight={50}
-            onKeyPress={(e: any) => {
-              if (e.shiftKey && e.charCode === 13) {
-                return true
-              }
-              if (e.charCode === 13) {
-                inputClearRef.current?.()
-                addMessage(215125215)
-              }
-            }}
-            rightButtons={
-              <Button text="Submit" onClick={() => addMessage(125125125)} />
-            }
-          />
-        </InputContainer>
-      </ChatViewContainer>
+      {/* Sizes are in px. DefaultSizes are scaled to ratio */}
+      <Allotment proportionalLayout={false}>
+        <Allotment.Pane
+          minSize={250}
+          maxSize={Infinity}
+          preferredSize={'250px'}
+        >
+          <ChatList />
+        </Allotment.Pane>
+        <Allotment.Pane>
+          <ChatViewContainer>
+            <MessageListContainer>
+              <MessageList />
+            </MessageListContainer>
+            <InputContainer>
+              <Input
+                className="rce-example-input"
+                placeholder="Write a message..."
+                defaultValue=""
+                multiline={true}
+                maxlength={1000}
+                onMaxLengthExceed={() => console.log('onMaxLengthExceed')}
+                reference={inputRef}
+                clear={(clear: any) => (inputClearRef.current = clear)}
+                maxHeight={50}
+                onKeyPress={(e: any) => {
+                  if (e.shiftKey && e.charCode === 13) {
+                    return true
+                  }
+                  if (e.charCode === 13) {
+                    inputClearRef.current?.()
+                    addMessage(215125215)
+                  }
+                }}
+                rightButtons={
+                  <Button text="Submit" onClick={() => addMessage(125125125)} />
+                }
+              />
+            </InputContainer>
+          </ChatViewContainer>
+        </Allotment.Pane>
+      </Allotment>
     </MainContainer>
   )
 }
